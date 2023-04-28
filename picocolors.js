6254e9d0d692c612571089357ce268815ac80a35
Romuald Brillout <git@brillout.com>
2023-04-28 22:06:23 +0200

    tmp: remove require call

--- picocolors.js
+++ picocolors.js
@@ -1,17 +1,8 @@
-let tty;
-{
-  const req = require
-  try {
-    tty = req("tty")
-  } catch {}
-}
-
 let isColorSupported =
 	!("NO_COLOR" in process.env || process.argv.includes("--no-color")) &&
 	("FORCE_COLOR" in process.env ||
 		process.argv.includes("--color") ||
 		process.platform === "win32" ||
-		(tty && tty.isatty(1) && process.env.TERM !== "dumb") ||
 		"CI" in process.env)
 
 let formatter =
