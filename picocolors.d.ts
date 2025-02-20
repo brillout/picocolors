92bced3be2ce705372084cd090ee3ad17fc3d0a3
Romuald Brillout <git@brillout.com>
2025-02-20 12:24:39 +0100

    fix: fix .d.ts

--- picocolors.d.ts
+++ picocolors.d.ts
@@ -1,5 +1,37 @@
-import { Colors } from "./types"
-
 declare const pc: Colors
 
-export = pc
+export default pc
+
+// export type Formatter = (input: string | number | null | undefined) => string
+type Formatter = <T extends string>(input: T) => T
+
+interface Colors {
+       isColorSupported: boolean
+       code: Formatter
+       string: Formatter
+       reset: Formatter
+       bold: Formatter
+       dim: Formatter
+       italic: Formatter
+       underline: Formatter
+       inverse: Formatter
+       hidden: Formatter
+       strikethrough: Formatter
+       black: Formatter
+       red: Formatter
+       green: Formatter
+       yellow: Formatter
+       blue: Formatter
+       magenta: Formatter
+       cyan: Formatter
+       white: Formatter
+       gray: Formatter
+       bgBlack: Formatter
+       bgRed: Formatter
+       bgGreen: Formatter
+       bgYellow: Formatter
+       bgBlue: Formatter
+       bgMagenta: Formatter
+       bgCyan: Formatter
+       bgWhite: Formatter
+}
